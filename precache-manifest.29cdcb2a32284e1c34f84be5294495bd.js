99c99536e1786f046f908c3cb208d44181a74895
Igor Shetser <ushiromiya7@gmail.com>
2020-10-12 00:58:52 +0300

    Updates

---
+++ precache-manifest.29cdcb2a32284e1c34f84be5294495bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bacb6d9c9149fc1bdd",
+    "url": "/af6df2eb4268e6981426.module.wasm"
+  },
+  {
+    "revision": "1f491fd90dc0692b8573492e51e0b6ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68950b1fa122c79c6103",
+    "url": "/static/css/main.af72dca5.chunk.css"
+  },
+  {
+    "revision": "b3bacb6d9c9149fc1bdd",
+    "url": "/static/js/0.17263783.chunk.js"
+  },
+  {
+    "revision": "60aae5e818bdebd5574b",
+    "url": "/static/js/3.27303a11.chunk.js"
+  },
+  {
+    "revision": "c5017c2d90a4dcd618e5",
+    "url": "/static/js/4.78284523.chunk.js"
+  },
+  {
+    "revision": "68950b1fa122c79c6103",
+    "url": "/static/js/main.bc03c05e.chunk.js"
+  },
+  {
+    "revision": "0ac60e9ece7b37e25974",
+    "url": "/static/js/runtime-main.e645e9a9.js"
+  }
+]);
